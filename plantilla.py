82db1fb70ee11cdc761ceaf5a49b2d86b80304c8
TheMightyTsar <fernandeznicolas0505@gmail.com>
2023-04-04 13:58:26 -0400

    final commit

--- plantilla.py
+++ plantilla.py
@@ -18,20 +18,67 @@ JorgePNG_scaled = None
 
 
 class Enemy(pygame.sprite.Sprite):
+
+    ###    def __init__(self):
+    ###        super(Enemy, self).__init__()
+    ###        self.surf = image1
+    ###        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+    ###
+    # la posicion inicial es generada aleatoriamente, al igual que la velocidad
+    ###        self.rect = self.surf.get_rect(
+    ###            center=(
+    ###                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+    ###                random.randint(0, SCREEN_HEIGHT),
+    ###            )
+    ###        )
+    ###        self.speed = random.randint(3, 5)
+
     def __init__(self):
         super(Enemy, self).__init__()
         pass
 
-
+    ###    def update(self):
+    ###        self.rect.move_ip(-self.speed, 0)
+    ###        if self.rect.right < 0:
+    ###            self.kill()
 
     def update(self):
         pass
 
 
 class Player(pygame.sprite.Sprite):
+
+    ### def __init__(self):
+    ###        # nos permite invocar métodos o atributos de Sprite
+    ###    super(Player, self).__init__()
+    ###    imageNotScaled = pygame.image.load('JorgeVJ.png')
+    ###    Jorge = pygame.transform.scale(imageNotScaled, (80, 80))
+    ###    self.surf = Jorge
+    ###    self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+    ###    self.rect = self.surf.get_rect()
+
     def __init__(self):
         super(Player, self).__init__()
         pass
+
+    ### def update(self, pressed_keys):
+    ###         if pressed_keys[K_UP]:
+    ###         self.rect.move_ip(0, -4)
+    ### if pressed_keys[K_DOWN]:
+    ###     self.rect.move_ip(0, 4)
+    ### if pressed_keys[K_LEFT]:
+    ###     self.rect.move_ip(-4, 0)
+    ### if pressed_keys[K_RIGHT]:
+    ###     self.rect.move_ip(4, 0)
+
+    ### if self.rect.left < 0:
+    ###     self.rect.left = 0
+    ###         if self.rect.right > SCREEN_WIDTH:
+    ###         self.rect.right = SCREEN_WIDTH
+    ###     if self.rect.top < 0:
+    ###         self.rect.top = 0
+    ###     if self.rect.bottom > SCREEN_HEIGHT:
+    ###         self.rect.bottom = SCREEN_HEIGHT
 
     def update(self):
         pass
@@ -53,7 +100,6 @@ SCREEN_HEIGHT = 700
 screen = None
 ### background_image = pygame.image.load("pixelBackground.jpg").convert()
 background_image = None
-
 
 ''' Preparamos el gameloop '''
 ''' 1.- creamos el reloj del juego'''
